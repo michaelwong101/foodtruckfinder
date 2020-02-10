09b1c08099a611ba7468ff2d27f157d9ae2365d1
Michael Wong <michaelwong101@gmail.com>
2020-02-09 18:45:32 -0800

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,5 @@
+## To compile
+In the folder `foodtruckfinder`  
+`nuget restore foodtruckfinder.sln`  
+`msbuild foodtruckfinder.sln`  
+`mono foodtruckfinder/bin/Debug/foodtruckfinder.exe`
---
+++ foodtruckfinder/FoodTruck.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace foodtruckfinder
+{
+    public class FoodTruck:IComparable<FoodTruck>
+    {
+        public bool IsOpen(DateTime t)
+        {
+            if(dayofweekstr == t.DayOfWeek.ToString())
+            {
+                if(t.CompareTo(starttime) >= 0 && t.CompareTo(endtime) < 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(applicant + " - " + location);
+        }
+
+        public int CompareTo(FoodTruck other)
+        {
+            return applicant.CompareTo(other.applicant);
+        }
+
+        public string dayofweekstr {get;set;}
+        public DateTime starttime {get;set;}
+        public DateTime endtime {get;set;}
+        public string location {get;set;}
+        public string locationdesc {get;set;}
+        public string applicant {get;set;}
+        public double latitude {get;set;}
+        public double longitude {get;set;}
+    }
+}
